4fe49d4bdd209914b6c523eb3408ad0fa8421154
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-10-18 04:40:41 -0400

    remove unused files

--- vwp-plugin/assets/scripts.js
+++
@@ -1,1 +0,0 @@
-const o="my-rifle";function i(){console.log("this is: ",o)}i();
--- vwp-plugin/includes/vwp-plugin-deactivate.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class VwpPluginDeactivate
-{
-  public static function deactivate() {
-    flush_rewrite_rules();
-  }
-}
