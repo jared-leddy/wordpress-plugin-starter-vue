17df0d3472aeeeabc87c5969d595598cb5ed70dd
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-10-17 23:23:37 -0400

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Hello WordPress with Vue 3!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss">
+#app {
+  h1 {
+    color: #42b983;
+  }
+}
+</style>
